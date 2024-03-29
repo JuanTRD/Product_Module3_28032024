0f3681b463d75f1b7b10d781381ac8747413f73e
locpham92 <phamquyloc92@gmail.com>
2024-03-29 19:53:52 +0700

    loc tao OrderService

--- src/main/java/Service/OrderService.java
+++ src/main/java/Service/OrderService.java
@@ -1,4 +1,27 @@
 package Service;
 
+import Model.Customer;
+import Model.Order;
+import Model.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class OrderService {
+    private List<Order> orderList = new ArrayList<>();
+    public OrderService() {}
+    public void creat(Order order) {}
+    public List<Order> viewAll() {
+        return null;
+    }
+    public int findIndexById(int id) {
+        return -1;
+    }
+    public Customer findCustomerbyOrder(int id) {
+        return null;
+    }
+    public Order findOrderbyCustomer(int id) {
+        return null;
+    }
+
 }
