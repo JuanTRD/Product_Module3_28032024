9cf9f56a9ba5ee6d878e175efed7ab9deaf2e545
locpham92 <phamquyloc92@gmail.com>
2024-04-01 14:47:14 +0700

    loc update username va password vao model va service

--- src/main/java/Model/User.java
+++ src/main/java/Model/User.java
@@ -4,15 +4,17 @@ import Model.subModel.Role;
 
 public class User {
     private int id;
-    private String name;
+    private String username;
+    private String password;
     private Role role;
 
     public User() {
     }
 
-    public User(int id, String name, Role role) {
+    public User(int id, String username, String password, Role role) {
         this.id = id;
-        this.name = name;
+        this.username = username;
+        this.password = password;
         this.role = role;
     }
 
@@ -24,12 +26,20 @@ public class User {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public Role getRole() {
