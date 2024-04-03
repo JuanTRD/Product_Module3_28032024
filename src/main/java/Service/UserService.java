e5eda02df5752ab42c4f23c5f7537adb8ead3556
locpham92 <phamquyloc92@gmail.com>
2024-04-03 18:35:26 +0700

    loc sua het

--- src/main/java/Service/UserService.java
+++ src/main/java/Service/UserService.java
@@ -13,17 +13,21 @@ import java.util.List;
 
 public class UserService {
     private Connection connection = ConnectToMySQL.getConnection();
+    public RoleService roleService = new RoleService();
+
+    private List<Role> roleList = new ArrayList<>();
+
     public UserService() {
     }
 
     public void addUser(User user) {
-        String sql = "insert into user(id,username, password,idRole) values (?, ?, ?, ?);";
+        String sql = "insert into user(?, ?, ?, ?);";
         try {
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
             preparedStatement.setInt(1, user.getId());
             preparedStatement.setString(2, user.getUsername());
             preparedStatement.setString(3, user.getPassword());
-            preparedStatement.setInt(4, user.getIdRole());
+            preparedStatement.setInt(4, roleService.getIdByRole(user.getIdRole()));
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -50,22 +54,35 @@ public class UserService {
         return userList;
     }
 
-    public boolean checkUser(String username, String password) {
-        String sql = "select * from user;";
+    public List<Role> viewAllRole() {
+        String sql = "select * from role;";
         try {
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
             ResultSet rs = preparedStatement.executeQuery();
             while (rs.next()) {
-                String username1 = rs.getString("username");
-                String password1 = rs.getString("password");
-                if (username1 == username && password1 == password) {
-                return true;
-                }
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                Role role = new Role(id, name);
+                roleList.add(role);
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        return roleList;
+    }
+
+    public boolean checkUser(String username, String password) {
+        for (int i = 0; i < userList.size(); i++) {
+            if (username.equals(userList.get(i).getUsername()) && password.equals(userList.get(i).getPassword())) {
+                return true;
+            }
+        }
         return false;
+    }
+
+    public int getIDRole(String username, String password) {
+
+        return null;
     }
 
     public void edit(int id, User user) {
@@ -78,22 +95,7 @@ public class UserService {
     public int findIndexById(int id) {
         return -1;
     }
-    public int getIdUser(String username, String password) {
-        String sql = "SELECT * from user where username =? and password=?;";
-        int id=-1;
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.setString(1, username);
-            preparedStatement.setString(2, password);
-            ResultSet rs = preparedStatement.executeQuery();
-            while (rs.next()) {
-                id = rs.getInt("id");
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return id;
-    }
+
     public User findById(int id) {
         String sql = "SELECT * from user where id =?;";
         User user = null;
@@ -104,8 +106,7 @@ public class UserService {
             while (rs.next()) {
                 String username = rs.getString("username");
                 String password = rs.getString("password");
-                Integer idRole = rs.getInt("idrole");
-                user = new User(id, username, password,idRole);
+                user = new User(id, username, password);
             }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -113,4 +114,7 @@ public class UserService {
         return user;
     }
 
+    public int getIdUser(String username, String password) {
+        ;
+    }
 }
